1e23c3fd5c0a08430ede7ed3c034d550fed343a5
Martin Loeper <martin.loeper@gmx.de>
2018-03-11 18:30:31 +0100

    Change order of ls

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -2,5 +2,5 @@
 pwd
 cd target
 zip product product
-ls -la
 cat extractor.exe marker.txt org.eclipse.oomph.extractor.lib.jar marker.txt product-descriptor marker.txt product.zip marker.txt > kamp-eclipse-inst-64.exe
+ls -la

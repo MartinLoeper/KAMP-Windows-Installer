c5f23303af7ae955505477cac3b757d425c68637
Martin Loeper <martin.loeper@gmx.de>
2018-03-11 18:19:46 +0100

    Examine the zip file

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -2,4 +2,6 @@
 pwd
 cd target
 zip product product
+ls -la
+ls product.zip
 cat extractor.exe marker.txt org.eclipse.oomph.extractor.lib.jar marker.txt product-descriptor marker.txt product.zip marker.txt > "../kamp-eclipse-inst-64.exe"

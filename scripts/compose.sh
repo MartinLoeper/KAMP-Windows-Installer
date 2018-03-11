2774d9513f371d010d4a6ea360e8b2d9203b2eb0
Martin Loeper <martin.loeper@gmx.de>
2018-03-11 18:08:27 +0100

    Fix ci build

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pwd
 cd target
-cat extractor.exe marker.txt + org.eclipse.oomph.extractor.lib.jar + marker.txt + product-descriptor + marker.txt + product.zip + marker.txt  > "../kamp-eclipse-inst-64.exe"
+zip product product
+cat extractor.exe marker.txt org.eclipse.oomph.extractor.lib.jar marker.txt product-descriptor marker.txt product.zip marker.txt > "../kamp-eclipse-inst-64.exe"

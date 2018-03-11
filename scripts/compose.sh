14ef4016b1ba1af5753b938fc213d103b1e3ffa1
Martin Loeper <martin.loeper@gmx.de>
2018-03-11 20:26:18 +0100

    Fix the zip command

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -2,6 +2,6 @@
 pwd
 cd target
 cd product
-zip -r product *
+zip -r ../product.zip *
 cd ..
 cat extractor.exe marker.txt org.eclipse.oomph.extractor.lib.jar marker.txt product-descriptor marker.txt product.zip marker.txt > kamp-eclipse-inst-64.exe

9c4652349d7dbab0b8d013d8ee17174b140b338d
Martin Loeper <martin.loeper@gmx.de>
2018-03-11 20:13:05 +0100

    Junk the zip path

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
 cd target
-zip -r product product
+zip -jr product product
 cat extractor.exe marker.txt org.eclipse.oomph.extractor.lib.jar marker.txt product-descriptor marker.txt product.zip marker.txt > kamp-eclipse-inst-64.exe
 ls -la

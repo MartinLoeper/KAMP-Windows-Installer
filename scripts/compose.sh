2e1459f305b4bf083441e7040bc4ded620a477f4
Martin Loeper <martin.loeper@gmx.de>
2018-03-11 18:35:36 +0100

    Make zip recursive

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
 cd target
-zip product product
+zip -r product product
 cat extractor.exe marker.txt org.eclipse.oomph.extractor.lib.jar marker.txt product-descriptor marker.txt product.zip marker.txt > kamp-eclipse-inst-64.exe
 ls -la

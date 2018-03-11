a4151ef25e0de582a32c0f37f271afd7fedb3e79
Martin Loeper <martin.loeper@gmx.de>
2018-03-11 18:22:17 +0100

    Examine the root directory

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -2,6 +2,5 @@
 pwd
 cd target
 zip product product
-ls -la
-ls product.zip
+ls -la ..
 cat extractor.exe marker.txt org.eclipse.oomph.extractor.lib.jar marker.txt product-descriptor marker.txt product.zip marker.txt > "../kamp-eclipse-inst-64.exe"
